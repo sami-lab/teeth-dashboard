93485ea4f6833feae93fe6a676e56e733ca4977f
Sami <s.m.sami125@gmail.com>
2022-05-10 20:48:55 +0500

    grid

--- src/component/dashboard/dashboard.js
+++ src/component/dashboard/dashboard.js
@@ -1,14 +1,34 @@
-import React from "react";
-import { Grid, Button } from "@material-ui/core";
+import React, { useState } from 'react';
+import {
+  Grid,
+  Button,
+  TableContainer,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+const useStyles = makeStyles((theme) => ({
+  teethFamily: {
+    fontSize: '10px',
+    textAlign: 'center',
+    border: 0,
+  },
+}));
 export default function Dashboard() {
+  const classes = useStyles();
+  const [selectedFamily, setSelectedFamily] = useState([]);
+
   const teethFamily = [
-    "IMPLANT",
-    "PROTHESE FIXE",
-    "PROVISOIRE",
-    "PIVOT",
-    "OBTURATION",
-    "ENDO",
-    "EXTRACTION",
+    'IMPLANT',
+    'PROTHESE FIXE',
+    'PROVISOIRE',
+    'PIVOT',
+    'OBTURATION',
+    'ENDO',
+    'EXTRACTION',
   ];
   const teethGroup1 = [
     18, 17, 16, 15, 14, 13, 12, 11, 21, 22, 23, 24, 25, 26, 27, 28,
@@ -29,18 +49,62 @@ export default function Dashboard() {
             <Button
               style={{
                 borderRadius: 0,
-                backgroundColor: "#F1D7FD",
-                color: "#2d2d2d",
-                marginTop: "15px",
-                textDecoration: "underline",
-                padding: "10px 25px",
+                backgroundColor: '#F1D7FD',
+                color: '#2d2d2d',
+                marginTop: '15px',
+                textDecoration: 'underline',
+                padding: '10px 25px',
               }}
             >
               SOINS COURANTS
             </Button>
           </Grid>
           {/* grid */}
-          <Grid item></Grid>
+          <Grid item>
+            <TableContainer>
+              <Table>
+                <TableBody>
+                  {teethFamily.map((row) => (
+                    <TableRow key={row.name}>
+                      <TableCell
+                        className={classes.teethFamily}
+                        component="th"
+                        scope="row"
+                      >
+                        {row}
+                      </TableCell>
+                      {teethGroup1.map((n, i) => (
+                        <TableCell
+                          style={{
+                            border: '1px solid #2d2d2d',
+                            borderLeft: i === 0 ? 0 : '1px solid #2d2d2d',
+                            borderRight:
+                              i === teethGroup1.length - 1
+                                ? 0
+                                : '1px solid #2d2d2d',
+                            cursor: 'pointer',
+                          }}
+                          onClick={() => {
+                            if (selectedFamily.includes(row + '-' + n)) {
+                              setSelectedFamily((f) =>
+                                f.filter((el) => el === row + '-' + n)
+                              );
+                            } else {
+                              setSelectedFamily((f) => [...f, row + '-' + n]);
+                            }
+                          }}
+                        >
+                          {selectedFamily.includes(row + '-' + n) && (
+                            <CheckBoxIcon />
+                          )}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
         </Grid>
       </Grid>
       {/* for solde */}
