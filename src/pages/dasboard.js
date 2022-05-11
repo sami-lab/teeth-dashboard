027eeb8d08d24505514c47f89413e00a2f48dbd3
Sami <s.m.sami125@gmail.com>
2022-05-11 22:42:51 +0500

    header

--- src/pages/dasboard.js
+++ src/pages/dasboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import Dashboard from '../component/dashboard/dashboard';
 import Sidebar from '../utils/sidebar';
+import Header from '../utils/header';
 export default function Dasboard() {
   return (
     <Grid container>
@@ -9,7 +10,15 @@ export default function Dasboard() {
         <Sidebar />
       </Grid>
       <Grid item style={{ flex: 1 }}>
+        <Grid container direction="column">
+          <Grid item>
+            <Header/>
+          </Grid>
+          <Grid item>
+            
         <Dashboard />
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
